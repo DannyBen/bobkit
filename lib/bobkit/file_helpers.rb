e173e8db675a1dd4fd748626e8579790e6aa06b3
DannyB <dannyb@telesofia.com>
2016-04-16 06:45:31 +0000

    file helpers: add copy_file and copy_folder

--- lib/bobkit/file_helpers.rb
+++ lib/bobkit/file_helpers.rb
@@ -1,13 +1,23 @@
 module Bobkit
   module FileHelpers
+    def create_folder(path)
+      FileUtils.mkdir_p(path) unless File.directory?(path)
+    end
+
+    def create_folder_for(path)
+      create_folder File.dirname(path)
+    end
+
     def create_file(path, content)
       create_folder_for path
       File.write path, content
     end
 
-    def create_folder_for(path)
-      dir = File.dirname path
-      FileUtils.mkdir_p(dir) unless File.directory?(dir)
+    def copy_file(source, target)
+      create_folder_for target
+      FileUtils.copy_entry source, target
     end
+    alias_method :copy_folder, :copy_file
+
   end
 end
--- spec/bobkit/file_helpers_spec.rb
+++ spec/bobkit/file_helpers_spec.rb
@@ -5,13 +5,12 @@ describe FileHelpers do
     FileUtils.rm_rf 'tmp/spec'
   end
 
-  it "creates a file with its parent folders" do
-    file = 'tmp/spec/we/are/the/champions.txt'
+  it "creates a folder" do
+    path  = 'tmp/spec/rock/on'
     
-    create_file file, 'Queen'
+    create_folder path
     
-    expect(File.exist?(file)).to eq true
-    expect(File.read(file)).to eq 'Queen'
+    expect(File.directory?(path)).to eq true    
   end
 
   it "creates a folder for a file" do
@@ -22,4 +21,31 @@ describe FileHelpers do
     
     expect(File.directory?(dir)).to eq true
   end
+
+  it "creates a file with its parent folders" do
+    file = 'tmp/spec/we/are/the/champions.txt'
+    
+    create_file file, 'Queen'
+    
+    expect(File.exist?(file)).to eq true
+    expect(File.read(file)).to eq 'Queen'
+  end
+
+  it "copies a file" do
+    target = 'tmp/spec/nested/folder/bob.png'
+
+    copy_file 'spec/fixtures/assets/bob.png', target
+
+    expect(File.exist?(target)).to eq true
+  end
+  
+  it "copies a folder" do
+    target = 'tmp/spec/deep/assets'
+
+    copy_folder 'spec/fixtures/assets', target
+
+    expect(File.directory?(target)).to eq true
+    expect(File.exist?("#{target}/bob.png")).to eq true
+  end
+
 end
