49acc800df6b78f164997e80c67fb3eeb5794af2
DannyB <dannyb@telesofia.com>
2016-06-03 16:31:11 +0000

    examples: change prints to comments

--- examples/06_markdown/example.rb
+++ examples/06_markdown/example.rb
@@ -1,14 +1,17 @@
 # Markdown
+
 require 'bobkit'
 include Bobkit::Tasks
 
-puts "---> Convert Markdown to HTML string"
+# Convert Markdown to HTML string
 html = markdown 'article'
 puts html
 
-puts "---> Convert Markdown to HTML string with layout"
+# Convert Markdown to HTML string with layout
 html = markdown 'article', layout: :default
 puts html
 
-puts "---> Convert Markdown to HTML file with layout"
+# Convert Markdown to HTML file with layout
 markdown 'article', layout: :default, output: 'article'
+
+# Output is in the output folder
--- examples/07_markdown_slim/example.rb
+++ examples/07_markdown_slim/example.rb
@@ -1,15 +1,17 @@
 # Markdown Slim
+
 require 'bobkit'
 require 'byebug'
 include Bobkit::Tasks
 
-# All options pass through to the slim render function"
-# so you can also use scope context"
+# All options pass through to the slim render function so you can 
+# also use scope context.
 nav_links = { 'Home': '/',  'Blog': '/blog' }
 html = markdown 'article', layout: :default, sidebar: nav_links
 
-# Or, set the scope usine the `scope` function
+# Or, set the scope using the `scope` function.
 scope sidebar: nav_links
 html = markdown 'article', layout: :default
 puts html
+
 
